dcc8bdc06e8357498a582044f71a0aa479068120
snacksquid <spilliams@gmail.com>
2023-06-16 15:47:37 -0500

    Configured webpack, jest and babel

---
+++ dist/index.9b260ed313a14790ffe0.js
@@ -0,0 +1,18 @@
+(self["webpackChunkbattleship"] = self["webpackChunkbattleship"] || []).push([["index"],{
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/main.js"));
+/******/ }
+]);
